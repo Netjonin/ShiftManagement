e53e54badbf99276ea6cc2b74016a7d61cc71d4d
taofeekaj <taofeekaj@examroom.ai>
2024-06-16 16:49:49 +0100

    File changes

--- obj/Debug/net8.0/ShiftManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ShiftManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShiftManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f51a729e60a4f297f51db545b7a4ff609b24cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a71e7bc6fd43b54ab0fddf815975ba08cc87285")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShiftManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShiftManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
