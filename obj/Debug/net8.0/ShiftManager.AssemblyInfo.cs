d7a7e3e67bc23265e646524cf616109c8c5d62a5
taofeekaj <taofeekaj@examroom.ai>
2024-06-22 11:31:44 +0100

    data folder moved to infrastructure

--- obj/Debug/net8.0/ShiftManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/ShiftManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShiftManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ff7627c7a98e4c31d4881802a220b5ec1c0f74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e43fa8830531cc0b2cf52a8c1eb4e9a3dc2959b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShiftManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShiftManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
